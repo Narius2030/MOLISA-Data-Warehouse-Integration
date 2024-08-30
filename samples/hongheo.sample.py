a5adef164f9f89ddefc0a6059a08d00cc2adb643
Narius2030 <buiducnhan15122003@gmail.com>
2024-08-30 16:28:56 +0700

    fix: CustomProviderFaker class

--- samples/hongheo.sample.py
+++ samples/hongheo.sample.py
@@ -3,81 +3,87 @@ import random
 from datetime import datetime
 import json
 import psycopg2
+from providers import CustomProviderFaker
 
 
-fake = Faker(locale='vi_VN')
-fake.add_provider(nations_provider)
-fake.add_provider(district_codes_provider)
 
+class GenerateSamplesNCC():
+    def __init__(self, config, locale='vi_VN') -> None:
+        self.config = config
+        self.cstprovider = CustomProviderFaker()
+        self.fake = Faker(locale=locale)
+        # add customized provider
+        self.fake.add_provider(self.cstprovider.nations_provider)
+        self.fake.add_provider(self.cstprovider.district_codes_provider)
 
-def generate_cmnd_number():
-    return ''.join(str(random.randint(0, 9)) for _ in range(12))
+    def generate_cmnd_number(self):
+        return ''.join(str(random.randint(0, 9)) for _ in range(12))
 
+    def generate_ncc_category(self):
+        data = {
+            'LS': 'Liệt sĩ',
+            'BMVNAH': 'Bà mẹ Việt Nam anh hùng',
+            'AHLLVTND': 'Anh hùng lực lượng vũ trang nhân dân',
+            'AHLD': 'Anh hùng lao động'
+        }
+        key = random.choice(list(data.keys()))
+        return key, data[key]
 
-def generate_ncc_category():
-    data = {
-        'LS': 'Liệt sĩ',
-        'BMVNAH': 'Bà mẹ Việt Nam anh hùng',
-        'AHLLVTND': 'Anh hùng lực lượng vũ trang nhân dân',
-        'AHLD': 'Anh hùng lao động'
-    }
-    key = random.choice(list(data.keys()))
-    return key, data[key]
+    def fake_ncc_profile(self, num_rows=10):
+        data = []
+        for _ in range(num_rows):
+            data.append((
+                f'AHLD{random.randint(100, 999)}',  # Profile code
+                self.generate_ncc_category()[0],  # Contributor category code
+                self.fake.name(),  # Họ tên
+                self.fake.date_of_birth(minimum_age=40, maximum_age=80),  # Date of birth
+                random.choice([True, False]),  # Sex
+                self.fake.nations(),  # Ethnic
+                self.generate_cmnd_number(),  # Identity card
+                '67',  # Province code
+                self.fake.district_codes(),  # District code
+                self.fake.date_between_dates(datetime(2022,1,1), datetime(2022,12,31)),  # Decide monthly date
+                self.fake.date_between_dates(datetime(2022,1,1), datetime(2022,12,31)),  # Decide yearly date
+                f'{random.randint(100000, 999999)}',  # Decide monthly number
+                f'{random.randint(100000, 999999)}',  # Decide yearly number
+                self.fake.date_between(datetime(2022,1,1), datetime(2022,12,31)), # Date of subsidy spending,
+                random.choice([True, False]),
+                datetime.now()
+            ))
+        return data
 
-
-def generate_fake_data(num_rows=10):
-    data = []
-    for _ in range(num_rows):
-        data.append((
-            f'AHLD{random.randint(100, 999)}',  # Profile code
-            generate_ncc_category()[0],  # Contributor category code
-            fake.name(),  # Họ tên
-            fake.date_of_birth(minimum_age=40, maximum_age=80),  # Date of birth
-            random.choice([True, False]),  # Sex
-            fake.nations(),  # Ethnic
-            generate_cmnd_number(),  # Identity card
-            '67',  # Province code
-            fake.district_codes(),  # District code
-            fake.date_between_dates(datetime(2022,1,1), datetime(2022,12,31)),  # Decide monthly date
-            fake.date_between_dates(datetime(2022,1,1), datetime(2022,12,31)),  # Decide yearly date
-            f'{random.randint(100000, 999999)}',  # Decide monthly number
-            f'{random.randint(100000, 999999)}',  # Decide yearly number
-            fake.date_between(datetime(2022,1,1), datetime(2022,12,31)), # Date of subsidy spending,
-            random.choice([True, False]),
-            datetime.now()
-        ))
-    return data
-
-
-def insert_postgres(config, insert_stmt, values):
-    with psycopg2.connect(
-        database="ncc",
-        user=f"{config['USER']}",
-        password=f"{config['PASSWORD']}",
-        host=f"{config['HOST_LOCAL']}",
-        port=f"{config['PORT']}"
-    ) as conn:
-        with conn.cursor() as cur:
-            for value in values:
-                try:
-                    cur.execute(insert_stmt, value)
-                except Exception as exc:
-                    cur.execute("ROLLBACK")
-                    print(str(exc))
+    def insert_postgres(self, insert_stmt, values):
+        with psycopg2.connect(
+            database="ncc",
+            user=f"{self.config['USER']}",
+            password=f"{self.config['PASSWORD']}",
+            host=f"{self.config['HOST_LOCAL']}",
+            port=f"{self.config['PORT']}"
+        ) as conn:
+            with conn.cursor() as cur:
+                for value in values:
+                    try:
+                        cur.execute(insert_stmt, value)
+                    except Exception as exc:
+                        cur.execute("ROLLBACK")
+                        print(str(exc))
 
 
 if __name__=='__main__':
-
-    # Ví dụ sử dụng:
-    fake_data = generate_fake_data(10)
-    # print(fake_data[:]) # In 5 dòng dữ liệu đầu tiên
-
     with open("./airflow/config.json", "r") as file:
             config = json.load(file)
             
+    generator = GenerateSamplesNCC(config=config)
+    
+    # generate samples
+    fake_data = generator.fake_ncc_profile(10)
+    print(fake_data[:5])
+            
+    # insert to postgresql
     insert_stmt = '''INSERT INTO profile."nccProfile"(profile_code, ncc_code, full_name, 
                                 birth_of_date, sex, ethnic, identity_number, 
                                 province_code, district_code, decided_monthly_date, decided_once_date, 
                                 decided_monthly_num, decided_once_num, start_subsidize, support_bhyt, created_date)
                     VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
-    insert_postgres(config, insert_stmt, fake_data)
+    generator.insert_postgres(config, insert_stmt, fake_data)
+    
--- samples/providers.py
+++ samples/providers.py
@@ -1,27 +1,26 @@
 from faker.providers import DynamicProvider
 
-class CustomeProvider():
+class CustomProviderFaker(DynamicProvider):
     def __init__(self) -> None:
-        self.nations_provider = DynamicProvider(
+        self.__nations_provider = DynamicProvider(
             provider_name="nations",
             elements=["Kinh",  "M'Nông", "Tày", "Thái", "E Đê", "Nùng", "Cơ Tu", "Tà Ôi", "Phù Lá"]
         )
-
-        self.district_codes_provider = DynamicProvider(
+        self.__district_codes_provider = DynamicProvider(
             provider_name="district_codes",
             elements=["660", "661", "662", "663", "664", "665", "666", "667"]
         )
         
     @property
     def nations_provider(self):
-        return self._nations_provider
+        return self.__nations_provider
     
     @property
     def district_codes_provider(self):
-        return self._district_codes_provider
+        return self.__district_codes_provider
     
     
 if __name__=='__main__':
-    cstprovider = CustomeProvider()
-    print(cstprovider.district_codes_provider())
+    cstprovider = CustomProviderFaker()
+    print(cstprovider.district_codes_provider)
         
